0682f9f30faa35e3889b9bfa4f1b78f7cfc428ef
congminh1230 <congminh2012002@gmail.com>
2021-11-23 18:17:20 +0700

    bai 5.2 uploat

--- dangNhap/addCart.php
+++ dangNhap/addCart.php
@@ -7,7 +7,7 @@ if (isset($_SESSION['products'])) {
   $key = $_GET['id'];
 
   $product =$products[$key];
-  // $product['product_amount'] = 1;
+  $product['product_amount'] = 1;
   $_SESSION['cart'][]=$product;
   header('Location:cart.php');
 }
